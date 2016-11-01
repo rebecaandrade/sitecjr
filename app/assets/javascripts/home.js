e7aa5ce05ce105ccc8cb21246513b93c48fb2348
Mateus Luiz Freitas Barros <mateuslfb@hotmail.com>
2016-11-01 20:45:01 +0000

    Scroll implementado

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+(function(){
+
+  $(document).ready(function(){
+
+    $("#quem-somos-botao").click(function(){
+      $("#who-we-are").animatescroll();
+    });
+
+    $("#portifolio-botao").click(function(){
+      $("#projects").animatescroll();
+    });
+
+    $("#membros-botao").click(function(){
+      $("#members").animatescroll();
+    });
+
+    $("#contato-botao").click(function(){
+      $("#contato").animatescroll();
+    });
+
+  });
+
+})();
