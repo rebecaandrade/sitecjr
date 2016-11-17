dd686ca3a5625d1d9ab49414b44cd7f380b05cf9
Mateus Luiz Freitas Barros <mateuslfb@hotmail.com>
2016-11-17 16:35:21 +0000

    Componentizando blocos do quem somos e arrumando o texto da home para celular

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,20 +2,20 @@
 
   $(document).ready(function(){
 
-    $("#quem-somos-botao").click(function(){
-      $("#who-we-are").animatescroll();
+    $(".quem-somos-botao").click(function(){
+      $("section[role=who-we-are]").animatescroll();
     });
 
-    $("#portifolio-botao").click(function(){
-      $("#projects").animatescroll();
+    $(".portifolio-botao").click(function(){
+      $("section[role=projects]").animatescroll();
     });
 
-    $("#membros-botao").click(function(){
-      $("#members").animatescroll();
+    $(".membros-botao").click(function(){
+      $("section[role=members]").animatescroll();
     });
 
-    $("#contato-botao").click(function(){
-      $("#contato").animatescroll();
+    $(".contato-botao").click(function(){
+      $("section[role=contact]").animatescroll();
     });
 
   });
