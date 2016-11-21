1df75118fe51b65df94f711136f3ea93ab7aa55d
Mateus Luiz Freitas Barros <mateuslfb@hotmail.com>
2016-11-21 16:22:03 +0000

    Arrumando scroll de membros

--- app/assets/javascripts/members_section.js
+++ app/assets/javascripts/members_section.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
       arrows: true,
       centerMode: true,
       slidesToShow: 3,
-      autoplay: true
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+            autoplay: false
+          }
+        }
+      ]
   });
 });
